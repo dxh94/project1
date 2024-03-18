b150206fba6e769b9497169a9ecd30b592832bfe
dxh94 <doducthuan17@gmail.com>
2024-03-18 22:02:57 +0700

    zoom dc

--- lib/view/screen2.dart
+++ lib/view/screen2.dart
@@ -3,7 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-import '../models/project_model.dart';
+import '../model/project_model.dart';
+
 
 class ProjectDetailsScreen extends StatefulWidget {
   final int projectId;
@@ -20,6 +21,9 @@ class _ProjectDetailsScreenState extends State<ProjectDetailsScreen> {
   double _rotation = 0.0;
   double _previousRotation = 0.0;
   int? _selectedImageIndex;
+
+  // Photos? _currentPhotos;
+  Frame? _currentFrame;
 
   @override
   void initState() {
@@ -55,15 +59,15 @@ class _ProjectDetailsScreenState extends State<ProjectDetailsScreen> {
       ),
       body: (_projectDetails == null)
           ? const Center(child: CircularProgressIndicator())
-          : GestureDetector(
-        onTapDown: _onTapDown,
-        onTapUp: _onTapUp,
-        onScaleStart: _onScaleStart,
-        onScaleUpdate: _onScaleUpdate,
+          : Container(
+        // onTapDown: _onTapDown,
+        // onTapUp: _onTapUp,
+        // onScaleStart: _onScaleStart,
+        // onScaleUpdate: _onScaleUpdate,
         child: Stack(
-          children: _projectDetails!.photos!.asMap().entries.map((entry) {
-            final index = entry.key;
-            final photo = entry.value;
+          children: _projectDetails!.photos!.map((item) {
+            final index = _projectDetails!.photos!.indexOf(item);
+            final photo = item;
             final imageUrl = photo.url!;
             final imageFrame = photo.frame!;
             final isSelected = index == _selectedImageIndex;
@@ -73,28 +77,32 @@ class _ProjectDetailsScreenState extends State<ProjectDetailsScreen> {
               child: Stack(
                 children: [
                   GestureDetector(
+                    onScaleUpdate:
+                    (details ){
+                      _onScaleUpdate(details,index);
+                    },
+                    onScaleStart: (details){
+                      _onScaleStart(details,index);
+                    },
                     onTap: () => _onImageTap(index),
-                    child: Transform.rotate(
-                      angle: _rotation,
-                      child: Transform.scale(
-                        scale: _scale,
-                        child: Image.network(
-                          imageUrl,
-                          width: imageFrame.width!.toDouble(),
-                          height: imageFrame.height!.toDouble(),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
+                    child: Image.network(
+                      imageUrl,
+                      width: imageFrame.width!.toDouble(),
+                      height: imageFrame.height!.toDouble(),
+                      fit: BoxFit.cover,
                     ),
                   ),
                   if (isSelected)
-                    Container(
-                      width: imageFrame.width!.toDouble(),
-                      height: imageFrame.height!.toDouble(),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.blue,
-                          width: 4.0,
+                    IgnorePointer(
+                      child: Container(
+                        width: imageFrame.width!.toDouble(),
+                        height: imageFrame.height!.toDouble(),
+                        decoration: BoxDecoration(
+                          // color: Colors.red
+                          border: Border.all(
+                            color: Colors.blue,
+                            width: 4.0,
+                          ),
                         ),
                       ),
                     ),
@@ -107,16 +115,38 @@ class _ProjectDetailsScreenState extends State<ProjectDetailsScreen> {
     );
   }
 
-  void _onScaleStart(ScaleStartDetails details) {
-    _previousScale = _scale;
-    _previousRotation = _rotation;
+  // void _onScaleStart(ScaleStartDetails details, int index) {
+  //   _previousScale = _scale;
+  //   _previousRotation = _rotation;
+  // }
+  void _onScaleStart(ScaleStartDetails details, int index){
+    _currentFrame = Frame(x: _projectDetails!.photos![index].frame!.x!,
+        y: _projectDetails!.photos![index].frame!.y!,
+        width: _projectDetails!.photos![index].frame!.width!,
+        height: _projectDetails!.photos![index].frame!.height!);
+    setState(() {
+
+    });
   }
 
-  void _onScaleUpdate(ScaleUpdateDetails details) {
+  void _onScaleUpdate(ScaleUpdateDetails details, int index) {
+    print("_currentPhotos!.frame!.width! ${_currentFrame!.width}");
+    if (details.pointerCount ==1){
+      // di chuyen
+      _projectDetails!.photos![index].frame!.x = _projectDetails!.photos![index].frame!.x! + details.focalPointDelta.dx;
+      _projectDetails!.photos![index].frame!.y =_projectDetails!.photos![index].frame!.y! +  details.focalPointDelta.dy;
+    }else if(details.pointerCount == 2) {
+      _projectDetails!.photos![index].frame!.width =  _currentFrame!.width! * details.scale;
+      _projectDetails!.photos![index].frame!.height = _currentFrame!.height! * details.scale;
+      _projectDetails!.photos![index].frame!.x =  _currentFrame!.x!  +  (_currentFrame!.width! -  _projectDetails!.photos![index].frame!.width!)/2;
+      _projectDetails!.photos![index].frame!.y =  _currentFrame!.y!  +  (_currentFrame!.height! -  _projectDetails!.photos![index].frame!.height!)/2;
+
+    }
     setState(() {
-      _scale = _previousScale * details.scale;
-      _rotation = _previousRotation + details.rotation;
     });
+      // _scale = _previousScale * details.scale;
+      // _rotation = _previousRotation + details.rotation;
+
   }
   void _onTapDown(TapDownDetails details) {
     final RenderBox box = context.findRenderObject() as RenderBox;
